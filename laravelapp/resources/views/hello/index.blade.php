c126c3c0fafde45f4017bbe460a1605388742d8a
1ke1144 <86399751+Kenta12340@users.noreply.github.com>
2022-12-03 12:00:24 +0900

    バリデーション150pまで

--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -8,9 +8,41 @@
 @endsection
 
 @section('content')
-    <p>ここが本文のコンテンツです。</p>
-    <p>これは、<middleware>google.com</middleware>へのリンクです。</p>
-    <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
+    <p>{{ $msg }}</p>
+    @if (count($errors) > 0)
+        <p>入力に問題があります。再入力して下さい。</p>
+    @endif
+        
+    <form action="/hello" method="POST">
+    <table>
+        @csrf
+        @error("name")
+            <tr><th>ERROR:</th><td>{{ $errors->first("name") }}</td></tr>
+        @endif
+        <tr>
+            <th>name:</th>
+            <td><input type="text" name="name" value="{{ old('name') }}"></td>
+        </tr>
+        @error("age")
+        <tr><th>ERROR:</th><td>{{ $errors->first("age") }}</td></tr>
+        @enderror
+        <tr>
+            <th>age:</th>
+            <td><input type="text" name="age" value="{{ old('age') }}"></td>
+        </tr>
+        @error("mail")
+        <tr><th>ERROR:</th><td>{{ $errors->first("mail") }}</td></tr>
+        @enderror
+        <tr>
+            <th>mail:</th>
+            <td><input type="text" name="mail" value="{{ old('mail') }}"></td>
+        </tr>
+        <tr>
+            <th></th>
+            <td><input type="submit" value="send"></td>
+        </tr>
+    </table>
+</form>
 @endsection
 
 @section('footer')
--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -19,8 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', 'App\Http\Controllers\HelloController@index')
-        ->middleware(HelloMiddleware::class);
+Route::get('hello', 'App\Http\Controllers\HelloController@index');
 Route::post('hello', 'App\Http\Controllers\HelloController@post');
 
 Route::get('top', 'App\Http\Controllers\HelloController@test');
