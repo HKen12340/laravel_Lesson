243a4708e97dc3436f6fcba2baefb86720c6fb85
1ke1144 <86399751+Kenta12340@users.noreply.github.com>
2022-12-03 17:48:38 +0900

    レスポンス162pまで

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -15,28 +15,7 @@ class HelloController extends Controller
       return view('hello.index',['msg'=>'フォームに入力']);
    }
 
-   public function post(Request $request){
-
-      $rules = [
-         'name' => 'required',
-         'mail' => 'email',
-         'age' => 'numeric|between:0,150'
-      ];
-
-      $messages = [
-         'name.required' => '名前は必ず入力してください',
-         'mail.email' => 'メールアドレスが必要です',
-         'age.numeric' => '年齢を整数で記入してください',
-         'age.between' => '年齢は0～150の間で入力してください'
-      ];
-      $validator = Validator::make($request->all(),$rules
-         ,$messages);
-
-      if ($validator->fails()){
-         return redirect('/hello')
-         ->withErrors($validator)
-         ->withInput();
-      }
+   public function post(HelloRequest $request){
          return view('hello.index',['msg' =>  '正しく入力されました!']);
    }
    public function test(){
--- laravelapp/app/Http/Requests/HelloRequest.php
+++ laravelapp/app/Http/Requests/HelloRequest.php
@@ -1,8 +1,8 @@
 <?php
-
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use App\Rules\Myrule;
 
 class HelloRequest extends FormRequest
 {
@@ -28,18 +28,18 @@ class HelloRequest extends FormRequest
     public function rules()
     {
         return [
-            // 'name'=>'required',
-            // 'mail'=>'email',
-            // 'age'=>'numeric|between:0,150',
+            'name'=>'required',
+            'mail'=>'email',
+            'age'=>['numeric', new Myrule(200)],
         ];
     }
 
-    // public function messages(){
-    //     return [
-    //         'name.required' => '名前は必ず入力して下さい',
-    //         'mail.email' => 'メールアドレスが必要です',
-    //         'age.numeric' => '年齢を整数で記入してください',
-    //         'age.between' => '年齢は0 ~ 150の間で入力してください。',
-    //     ];
-    // }
+    public function messages(){
+        return [
+            'name.required' => '名前は必ず入力して下さい',
+            'mail.email' => 'メールアドレスが必要です',
+            'age.numeric' => '年齢を整数で記入してください',
+            'age.hello' => 'hello! 入力は偶数のみ受け付けます',
+        ];
+    }
 }
